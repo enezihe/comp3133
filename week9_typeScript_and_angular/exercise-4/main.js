e35a5010e51212a474190881d158da7515353ea7
enezihe <enezihe@gmail.com>
2026-03-11 01:39:26 -0400

    add Lab06 TypeScript exercise 4

---
+++ week9_typeScript_and_angular/exercise-4/main.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var customer_1 = require("./customer");
+var customer = new customer_1.Customer("Jane", "User", 25);
+customer.greeter();
+customer.GetAge();
---
+++ week9_typeScript_and_angular/exercise-4/main.ts
@@ -0,0 +1,6 @@
+import { Customer } from "./customer";
+
+let customer = new Customer("Jane", "User", 25);
+
+customer.greeter();
+customer.GetAge();
