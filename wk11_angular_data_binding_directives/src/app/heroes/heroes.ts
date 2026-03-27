b17065d7ee97f204c3d4f046fb72a1e53f907e6d
enezihe <enezihe@gmail.com>
2026-03-27 05:21:21 -0400

    Lab08 Exercise 1 - Angular Data Binding & Directives (Tour of Heroes)

---
+++ wk11_angular_data_binding_directives/src/app/heroes/heroes.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { NgFor, NgIf, UpperCasePipe } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.html',
+  styleUrl: './heroes.css',
+  imports: [NgFor, NgIf, UpperCasePipe, FormsModule]
+})
+export class Heroes {
+  heroes = HEROES;
+  selectedHero?: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+}
